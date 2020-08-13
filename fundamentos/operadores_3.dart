7a59aefcb49d870eba0222951c7376f1012811e1
Bárbara Oliveira da Silva <barbara@ciandt.com>
2020-08-13 18:43:37 -0300

    operadores parte 4

---
+++ fundamentos/operadores_3.dart
@@ -0,0 +1,19 @@
+import 'dart:_internal';
+
+main() {
+  int a = 3;
+  int b = 4;
+
+  // Operadores Unários
+  a++; // Posfix
+  --a; // Prefix
+  print(a);
+
+  a--;
+  print(a);
+  print(a++ == --b); //Fuja de código complicado
+  print(a == b);
+
+  print(!true);
+  print(!false);
+}
